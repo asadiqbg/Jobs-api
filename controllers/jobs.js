141045451ccf8630917d561a2dfdbd908413c52a
asadiqbg <asadiqbg2000@gmail.com>
2025-07-20 22:32:13 +0500

    Creating Job routes

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -4,26 +4,58 @@ const {NotFoundError,BadRequestError} = require('../errors/index')
 
 
 const getAllJobs = async(req,res)=>{
-  res.send('get all jobs')
+  const allJobs = await Job.find({createdBy:req.user.userId}).sort('createdAt')
+  res.status(StatusCodes.OK).json({allJobs})
 }
 
 const getJobs = async(req,res)=>{
-  res.send('get jobs')
+  const {
+    user:{userId},
+    params:{id:jobId}
+  } = req
+  const job = await Job.findOne({_id:jobId,createdBy:userId})
+  if(!job){
+    throw new NotFoundError(`Job with id ${jobId} not found`)
+  }
+
+  res.status(StatusCodes.OK).json({job})
 }
 
 const createJobs = async(req,res)=>{
   req.body.createdBy = req.user.userId
   const job= await Job.create(req.body)
-  console.log(req.user.userId)
   res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJobs = async(req,res)=>{
-  res.send('update jobs')
+  const {
+    body:{company,position},
+    user:{userId},
+    params:{id:jobId}
+  } = req
+  if(company === '' || position === ''){
+    throw new BadRequestError('Company or position field should be provided')
+  }
+  const job = await Job.findByIdAndUpdate({_id:jobId,createdBy:userId},req.body,{new:true,runValidators:true})
+  if(!job){
+    throw new NotFoundError(`No job found with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({job})
 }
 
 const deleteJobs = async(req,res)=>{
-  res.send('delete jobs')
+
+  const {
+    user:{userId},
+    params:{id:jobId}
+  } = req
+  
+  const job =  await Job.findOneAndDelete({_id:jobId,createdBy:userId})
+  if(!job){
+    throw new NotFoundError(`No job found with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).send()
+
 }
 
 module.exports = {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,17 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode:err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong, Please try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message || 'Something went wrong' })
+
+  if(err.code || err.code === 11000){
+    customError.msg = `Duplicate value provided for ${Object.keys(err.keyValue)} field, please provide other value`
+    customError.statusCode = 400
+  }
+
+  return res.status(customError.statusCode).json({msg:customError.msg})
 }
 
 module.exports = errorHandlerMiddleware

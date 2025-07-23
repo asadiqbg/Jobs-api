71deca742ea29d72c8bad2046acf8746e093750d
asadiqbg <asadiqbg2000@gmail.com>
2025-07-23 20:23:16 +0500

    Adding Validation and Cast Errors in Errorhandler middleware

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -6,9 +5,21 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong, Please try again later'
   }
 
-  if(err.code || err.code === 11000){
-    customError.msg = `Duplicate value provided for ${Object.keys(err.keyValue)} field, please provide other value`
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',')
     customError.statusCode = 400
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`
+    customError.statusCode = 400
+  }
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id : ${err.value}`
+    customError.statusCode = 404
   }
 
   return res.status(customError.statusCode).json({msg:customError.msg})
